41ac293a0edb1205f43e773e71e5543f4ff29175
Xarianne <games.clinking002@passinbox.com>
2025-07-23 10:46:42 +0100

    mesa-git

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,11 +8,68 @@ set -ouex pipefail
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+#
+# dnf5 -y copr enable danayer/libdrm-git
+dnf5 -y copr enable danayer/libdrm-git:ml # i386
+dnf5 -y copr enable danayer/linux-firmware-git
+dnf5 -y copr enable danayer/linux-firmware-git:ml # i386
+dnf5 -y copr enable danayer/mesa-git
+dnf5 -y copr enable danayer/mesa-git:ml # i386
+dnf5 -y copr enable danayer/virglrenderer-git
+dnf5 -y copr enable danayer/virglrenderer-git:ml # i386
+dnf5 -y copr enable danayer/Vulkan-Git
+dnf5 -y copr enable danayer/Vulkan-Git:ml # i386
 
-# this installs a package from fedora repos
+### Handle specific conflicts and install mesa-git components
+
+# Explicitly remove mesa-va-drivers-freeworld
+# This is a common conflict as mesa-git provides its own VA-API drivers.
+# `--allowerasing` might handle this, but explicit removal is safer for 'freeworld' packages.
+dnf5 -y remove mesa-va-drivers-freeworld || true # Use '|| true' to prevent script from failing if package isn't present for some reason
+
+# Install the core mesa-git, virglrenderer-git, and vulkan-git packages
+# `--allowerasing` will handle the 'mesa-filesystem' and other direct replacements.
+# `--setopt=install_weak_deps=False` keeps the image clean.
+dnf5 install -y \
+  mesa-dri-drivers \
+  mesa-libgl-devel \
+  mesa-vulkan-drivers \
+  virglrenderer \
+  vulkan-loader \
+  vulkan-tools \
+  --allowerasing \
+  --setopt=install_weak_deps=False
+
+# VA-API and VDPAU drivers from mesa-git:
+# (These usually come with mesa-git, but explicit installation can ensure them if missing)
+dnf5 install -y \
+  mesa-va-drivers \
+  mesa-vdpau-drivers \
+  --allowerasing \
+  --setopt=install_weak_deps=False
+
+### Other system cleanup/config
+
+# Uninstall iBus (as per your original request)
+dnf5 -y remove ibus
+
+# Programs to install
 dnf5 install -y \
      steam \
      lutris
+
+### Disable COPRs
+dnf5 -y copr disable danayer/libdrm-git
+dnf5 -y copr disable danayer/libdrm-git:ml
+dnf5 -y copr disable danayer/linux-firmware-git
+dnf5 -y copr disable danayer/linux-firmware-git:ml
+dnf5 -y copr disable danayer/mesa-git
+dnf5 -y copr disable danayer/mesa-git:ml
+dnf5 -y copr disable danayer/virglrenderer-git
+dnf5 -y copr disable danayer/virglrenderer-git:ml
+dnf5 -y copr disable danayer/Vulkan-Git
+dnf5 -y copr disable danayer/Vulkan-Git:ml
+
 
 # Use a COPR Example:
 #
