ed2d50ced458fd353809481b5f2f6f3e80bf150f
Xarianne <games.clinking002@passinbox.com>
2025-07-28 16:12:36 +0100

    Feat: attempting to install mesa-git drivers

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,7 +2,21 @@
 
 set -ouex pipefail
 
-### Enable COPRs
+# Enable COPRs
+dnf5 -y copr enable danayer/Vulkan-Git
+dnf5 -y copr enable danayer/libdrm-git
+dnf5 -y copr enable danayer/linux-firmware-git
+dnf5 -y copr enable danayer/mesa-git
+dnf5 -y copr enable danayer/virglrenderer-git
+
+# === Atomically swap the drivers ===
+# Using one explicit swap command without variables.
+# Note: This list is a best guess. If this step fails, you will
+# need to check the build log for the exact conflicting packages.
+dnf5 swap -y \
+    --allowerasing \
+    mesa-dri-drivers mesa-vulkan-drivers mesa-libglapi libdrm linux-firmware vulkan-loader virglrenderer \
+    mesa-dri-drivers-git mesa-vulkan-drivers-git mesa-libglapi-git libdrm-git linux-firmware-git vulkan-loader-git virglrenderer-git
 
 # Programs to remove
 dnf5 remove -y \
@@ -12,5 +26,12 @@ dnf5 remove -y \
 dnf5 install -y \
     plasma-discover \
     plasma-discover-flatpak
+
+# Disable COPRs
+dnf5 -y copr disable danayer/Vulkan-Git
+dnf5 -y copr disable danayer/libdrm-git
+dnf5 -y copr disable danayer/linux-firmware-git
+dnf5 -y copr disable danayer/mesa-git
+dnf5 -y copr disable danayer/virglrenderer-git
 
 systemctl enable podman.socket
