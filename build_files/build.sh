617edfe748cf84813359b252c7907bc4fb8ab354
Xariann <130084241+Xarianne@users.noreply.github.com>
2025-10-29 15:37:55 +0000

    + virt manager and QEMU

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,7 +12,9 @@ set -ouex pipefail
 # this installs a package from fedora repos
 dnf5 install -y \
 steam \
-xone
+virt-manager \
+libvirt \
+qemu-kvm \
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
