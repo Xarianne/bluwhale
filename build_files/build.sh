fc14bf9c1ebafa8b2fc651771deabdb184569ca0
Xariann <xariann@archbtw>
2025-11-02 19:57:37 +0000

    Checking build conflicts

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,11 +10,12 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y \
-lutris \
-mangohud \
-input-remapper \
-kvantum
+# dnf5 install -y \
+# steam \
+# lutris \
+# mangohud \
+# input-remapper \
+# kvantum
 
 # Use a COPR Example:
 #
