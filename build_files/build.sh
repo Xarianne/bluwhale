2e07ac66c2c6e80623237e468361f9990d80958f
Xariann <130084241+Xarianne@users.noreply.github.com>
2025-11-03 07:40:32 +0000

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,8 +10,8 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y \
-steam \
+# dnf5 install -y \
+# steam \
 # lutris \
 # mangohud \
 # input-remapper \
