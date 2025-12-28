075cecf154befe8c954b2c89b57866c35f4377d9
Xariann <gitlab.unpack893@passmail.com>
2025-12-28 21:34:15 +0000

    Disabled RPM Fusion

--- build_files/kinoite-build.sh
+++ build_files/kinoite-build.sh
@@ -10,17 +10,6 @@ dnf5 install -y terra-release-extras
 dnf5 install -y terra-release-mesa
 dnf5 install -y terra-release-multimedia
 
-# Enable RPM Fusion
-dnf5 install -y \
-  https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
-  https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-
-# Repository priorities to avoid conflicts
-dnf5 config-manager setopt terra.priority=50
-dnf5 config-manager setopt terra-mesa.priority=50
-dnf5 config-manager setopt rpmfusion-free.priority=90
-dnf5 config-manager setopt rpmfusion-nonfree.priority=90
-
 ### PACKAGE INSTALLATION
 
 # Gaming tools and utilities
@@ -30,7 +19,7 @@ dnf5 install -y \
   input-remapper \
   mangohud \
   vkBasalt \
-  just \
+  just
 
 # Faugus Launcher, repo + package
 dnf5 -y copr enable faugus/faugus-launcher
@@ -42,17 +31,10 @@ dnf5 -y copr enable lilay/topgrade
 dnf5 -y install topgrade
 dnf5 -y copr disable lilay/topgrade
 
-# Tools
+# Docker to allow Winboat
 dnf5 install -y \
   docker \
   docker-compose \
-  # gcc \
-  # gcc-c++ \
-  # make \
-  # pkg-config \
-  # openssl-devel \
-  # rust \
-  # cargo 
 
 # Use a COPR Example:
 #
