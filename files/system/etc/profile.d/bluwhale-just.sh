735c435f936272a3cfd149eb234131ef7003951a
Xarianne <git config --global user.email>
2026-01-13 16:50:30 +0000

    added virtualization

--- files/system/etc/profile.d/bluwhale-just.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# System-wide alias for bluwhale just recipes
-
-# Point directly to the MASTER Justfile
-alias ujust='just --justfile /usr/share/bluwhale/just/Justfile --working-directory ~'
-
-# The user-friendly entry point
-alias show-menu='ujust menu'
